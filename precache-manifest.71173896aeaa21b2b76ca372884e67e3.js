e06ee3da4c13547b0d20268490f9ad071477c595
Jyotirmaya Sahu <jyotirmayasahu38@gmail.com>
2020-11-01 22:52:41 +0530

    Updates

---
+++ precache-manifest.71173896aeaa21b2b76ca372884e67e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fa0a6a282577337e17382485e9ad58",
+    "url": "/MoviSch/index.html"
+  },
+  {
+    "revision": "7b7829e0d64875e966f7",
+    "url": "/MoviSch/static/css/main.8014e31f.chunk.css"
+  },
+  {
+    "revision": "4ef89e2a637bbb1b141e",
+    "url": "/MoviSch/static/js/2.baa0da23.chunk.js"
+  },
+  {
+    "revision": "28f0b8d5b390aa0fe7256bb817e49e90",
+    "url": "/MoviSch/static/js/2.baa0da23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7829e0d64875e966f7",
+    "url": "/MoviSch/static/js/main.8e51862a.chunk.js"
+  },
+  {
+    "revision": "28a63bbda58c1bf12ab7",
+    "url": "/MoviSch/static/js/runtime-main.cfc9394c.js"
+  },
+  {
+    "revision": "040551d9f6d51b7790f96af0b51b311a",
+    "url": "/MoviSch/static/media/HomePageLogoDesktop.040551d9.svg"
+  },
+  {
+    "revision": "930981509d77229e9b21a4d35502081c",
+    "url": "/MoviSch/static/media/HomePageLogoMobile.93098150.svg"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/MoviSch/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "695fc692e185f3ce953094f794d51f5e",
+    "url": "/MoviSch/static/media/rolling-loader.695fc692.svg"
+  }
+]);
